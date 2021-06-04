98b62187268d356604e6e6f62050bca1c9d8eb0a
patricksykes <52962150+patricksykes@users.noreply.github.com>
2021-06-04 11:46:36 +1000

    Add requirements, change gamma, fix temperature dependence

--- runzoomizerparallel.R
+++ runzoomizerparallel.R
@@ -1,3 +1,8 @@
+require(mizer)
+require(tidyverse)
+require(foreach)
+require(assertthat)
+
 ZooMizer_coupled <- function(ID, tmax = 25, effort = 0) {
   groups <- read.csv("data/TestGroups_mizer.csv") #load in ZooMSS groups
   zoo_groups <- groups[which(groups$Type == "Zooplankton"),] #just keep the zooplankton
@@ -33,7 +38,7 @@ ZooMizer_coupled <- function(ID, tmax = 25, effort = 0) {
                                 alpha = 0.25, # * temp_eff, #takes care of assimilation efficiency & temp effect on Encounter
                                 kappa = exp(intercept), #10^(input$phyto_int) * 10^-1,
                                 lambda = 1 - slope, # 1 - input$phyto_slope,
-                                gamma = 4000 * temp_eff, #takes care of temp effect on PredRate and Encounter
+                                gamma = 640 * temp_eff, #takes care of temp effect on PredRate and Encounter
                                 # f0 = 0.6,
                                 # h = 10^50,
                                 R_factor = 1.01, #RMax = RFactor * RDI, default 4. Note RDI 
@@ -57,7 +62,8 @@ ZooMizer_coupled <- function(ID, tmax = 25, effort = 0) {
     ) %>%
     setResource(resource_dynamics = "resource_zooMizer")
   
-  fish_params <- setRateFunction(fish_params, "PredRate", "PredRate_temp")
+  # this line not needed if gamma is set to be temperature-dependent
+  # fish_params <- setRateFunction(fish_params, "PredRate", "PredRate_temp")
   
   fish_params@other_params$temp_eff <- 2.^((input$sst - 30)/10)
   
@@ -72,48 +78,48 @@ return(project(fish_params, t_max = tmax, dt = 0.1, effort = effort))
 }  
 
 
-PredRate_temp <- function (params, n, n_pp, n_other, t, feeding_level, ...) 
-  {
-    no_sp <- dim(params@interaction)[1]
-    no_w <- length(params@w)
-    no_w_full <- length(params@w_full)
-    if (length(params@ft_pred_kernel_p) == 1) {
-      n_total_in_size_bins <- sweep(n, 2, params@dw, "*", 
-                                    check.margin = FALSE)
-      pred_rate <- sweep(params@pred_kernel, c(1, 2), (1 - 
-                                                         feeding_level) * params@search_vol * n_total_in_size_bins, 
-                         "*", check.margin = FALSE)
-      pred_rate <- colSums(aperm(pred_rate, c(2, 1, 3)), dims = 1)
-      return(pred_rate * params@other_params$temp_eff)
-    }
-    idx_sp <- (no_w_full - no_w + 1):no_w_full
-    Q <- matrix(0, nrow = no_sp, ncol = no_w_full)
-    Q[, idx_sp] <- sweep((1 - feeding_level) * params@search_vol * 
-                           n, 2, params@dw, "*")
-    pred_rate <- Re(t(mvfft(t(params@ft_pred_kernel_p) * mvfft(t(Q)), 
-                            inverse = TRUE)))/no_w_full
-    pred_rate[pred_rate < 1e-18] <- 0
-    return(pred_rate * params@ft_mask * params@other_params$temp_eff)
-}
+# PredRate_temp <- function (params, n, n_pp, n_other, t, feeding_level, ...) 
+#   {
+#     no_sp <- dim(params@interaction)[1]
+#     no_w <- length(params@w)
+#     no_w_full <- length(params@w_full)
+#     if (length(params@ft_pred_kernel_p) == 1) {
+#       n_total_in_size_bins <- sweep(n, 2, params@dw, "*", 
+#                                     check.margin = FALSE)
+#       pred_rate <- sweep(params@pred_kernel, c(1, 2), (1 - 
+#                                                          feeding_level) * params@search_vol * n_total_in_size_bins, 
+#                          "*", check.margin = FALSE)
+#       pred_rate <- colSums(aperm(pred_rate, c(2, 1, 3)), dims = 1)
+#       return(pred_rate * params@other_params$temp_eff)
+#     }
+#     idx_sp <- (no_w_full - no_w + 1):no_w_full
+#     Q <- matrix(0, nrow = no_sp, ncol = no_w_full)
+#     Q[, idx_sp] <- sweep((1 - feeding_level) * params@search_vol * 
+#                            n, 2, params@dw, "*")
+#     pred_rate <- Re(t(mvfft(t(params@ft_pred_kernel_p) * mvfft(t(Q)), 
+#                             inverse = TRUE)))/no_w_full
+#     pred_rate[pred_rate < 1e-18] <- 0
+#     return(pred_rate * params@ft_mask * params@other_params$temp_eff)
+# }
 
 
 library(doParallel)
 cl <- makePSOCKcluster(max(1, detectCores()-1))     ## set up cores-1 machines
 registerDoParallel(cl, cores = (max(1, detectCores()-1)))
-clusterEvalQ(cl, library(mizer, assertthat, tidyverse)) %>% invisible()
+clusterEvalQ(cl, lapply(c("mizer", "assertthat", "tidyverse"), require, character.only = TRUE)) %>% invisible()
 clusterExport(cl, c("ZooMizer_coupled", "PredRate_temp"))
 
 
 
-sims5gamma4000temp <- foreach(ID = 1:20,
+sims5gamma640temp <- foreach(ID = 1:20,
                                  .packages = c("mizer", "assertthat", "tidyverse")
                                  #.export = c("ZooMizer_coupled", "PredRate_temp")
 ) %dopar% {
   source("ZooMizerResourceFunctions.R")
-  ZooMizer_coupled(ID, tmax = 15, effort = 0)
+  ZooMizer_coupled(ID, tmax = 100, effort = 0)
 }
 
-biomasses5fishgammadefault <- foreach(i=1:20) %dopar% sum(tail(getBiomass(sims5fishgammadefault[[i]]),1))
+biomasses5gamma640temp <- foreach(i=1:20) %dopar% sum(colMeans(tail(getBiomass(sims5gamma640temp[[i]]),25)))
 
 # plot(sim)
 # animateSpectra(sim)
