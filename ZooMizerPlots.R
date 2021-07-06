2e1ab3cdc44278e7cf646959ba4d8fcb1bd4ba67
patricksykes <52962150+patricksykes@users.noreply.github.com>
2021-07-06 14:32:16 +1000

    Introduce functions to get and plot abundance timeseries

--- ZooMizerPlots.R
+++ ZooMizerPlots.R
@@ -178,7 +178,7 @@ plot_spectra_zoomizer <- function(fish_params, zoo_params, n, n_pp, n_zoo,
   
   linecolours <- zoo_params@species_params$PlotColour
   names(linecolours) <- zoo_params@species_params$species
-  linecolours <- c(linecolours, sim@params@linecolour)
+  linecolours <- c(linecolours, fish_params@linecolour)
   
   # Create plot
   
@@ -268,7 +268,6 @@ plotBiomass_ZooMizer <- function (sim, zoo_params, species = NULL, start_time, e
           ylim = c(NA, NA), total = FALSE, background = FALSE, highlight = NULL, 
           ...) 
 {
-  browser()
   species <- valid_species_arg(sim, species)
   if (missing(start_time)) 
     start_time <- as.numeric(dimnames(sim@n)[[1]][1])
@@ -314,4 +313,76 @@ plotlyBiomass_ZooMizer <- function (sim, zoo_params, species = NULL, start_time,
 {
   argg <- c(as.list(environment()), list(...))
   ggplotly(do.call("plotBiomass_ZooMizer", argg))
+}
+
+getAbundanceFrame_ZooMizer <- function (sim, zoo_params, species = NULL, start_time = as.numeric(dimnames(sim@n)[[1]][1]), 
+                                      end_time = as.numeric(dimnames(sim@n)[[1]][dim(sim@n)[1]]), 
+                                      ylim = c(NA, NA), total = FALSE, ...) 
+{
+  species <- valid_species_arg(sim@params, species)
+  b <- getAbundance_ZooMizer(sim, zoo_params, ...)
+  if (start_time >= end_time) {
+    stop("start_time must be less than end_time")
+  }
+  b <- b[(as.numeric(dimnames(b)[[1]]) >= start_time) & (as.numeric(dimnames(b)[[1]]) <= 
+                                                           end_time), , drop = FALSE]
+  b_total <- rowSums(b)
+  if (total) {
+    b <- cbind(b, Total = b_total)
+    species <- c("Total", species)
+  }
+  bm <- reshape2::melt(b)
+  min_value <- 1e-20
+  bm <- bm[bm$value >= min_value & (is.na(ylim[1]) | bm$value >= 
+                                      ylim[1]) & (is.na(ylim[2]) | bm$value <= ylim[2]), ]
+  names(bm) <- c("Year", "Species", "Biomass")
+  species_levels <- c(zoo_params@species_params$species, dimnames(sim@n)$sp, "Background", 
+                      "Resource", "Total")
+  bm$Species <- factor(bm$Species, levels = species_levels)
+  # bm <- bm[bm$Species %in% species, ]
+  return(bm)
+}
+
+
+plotAbundance_ZooMizer <- function (sim, zoo_params, species = NULL, start_time, end_time, y_ticks = 6, 
+                                  ylim = c(NA, NA), total = FALSE, background = FALSE, highlight = NULL, 
+                                  ...) 
+{
+  species <- valid_species_arg(sim, species)
+  if (missing(start_time)) 
+    start_time <- as.numeric(dimnames(sim@n)[[1]][1])
+  if (missing(end_time)) 
+    end_time <- as.numeric(dimnames(sim@n)[[1]][dim(sim@n)[1]])
+  bm <- getAbundanceFrame_ZooMizer(sim, zoo_params, species = dimnames(sim@n)$sp, 
+                                 start_time = start_time, end_time = end_time, ylim = ylim, 
+                                 total = total)#, ...)
+  spec_bm <- bm[bm$Species %in% c("Total", zoo_params@species_params$species, species), ]
+  x_label <- "Year"
+  y_label <- "Abundance"
+  linecolours <- zoo_params@species_params$PlotColour
+  names(linecolours) <- zoo_params@species_params$species
+  linecolours <- c(linecolours, sim@params@linecolour)
+  p <- ggplot(spec_bm, aes(x = Year, y = Biomass)) +
+    scale_y_continuous(trans = "log10", 
+                       breaks = mizer:::log_breaks(n = y_ticks),
+                       labels = prettyNum, 
+                       name = y_label) +
+    scale_x_continuous(name = x_label) + 
+    scale_colour_manual(values = linecolours)
+  # scale_linetype_manual(values = sim@params@linetype)
+  if (background) {
+    back_sp <- dimnames(sim@n)$sp[is.na(sim@params@A)]
+    back_bm <- bm[bm$Species %in% back_sp, ]
+    if (nrow(back_bm) > 0) {
+      p <- p + geom_line(aes(group = Species), data = back_bm, 
+                         colour = sim@params@linecolour["Background"], 
+                         linetype = sim@params@linetype["Background"])
+    }
+  }
+  linesize <- rep(0.8, length(sim@params@linetype))
+  names(linesize) <- names(sim@params@linetype)
+  linesize[highlight] <- 1.6
+  p <- p + # scale_size_manual(values = linesize) +
+    geom_line(aes(colour = Species)) #, linetype = Species)) #, size = Species))
+  return(p)
 }
--- ZooMizerSummaryFunctions.R
+++ ZooMizerSummaryFunctions.R
@@ -15,3 +15,19 @@ getBiomass_ZooMizer <- function (sim, zoo_params, ...)
   return(cbind(zoo_biomass, fish_biomass))
 }
 
+getAbundance_ZooMizer <- function (sim, zoo_params, ...) 
+{
+  assert_that(is(sim, "MizerSim"))
+  assert_that(is(zoo_params, "MizerParams"))
+  zoo_size_range <- get_size_range_array(zoo_params, ...)
+  zoo_n <- aperm(array(unlist(sim@n_other), c(nrow(zoo_params@species_params),length(zoo_params@w),length(sim@n_other))), c(3,1,2))
+  dimnames(zoo_n) <- list(getTimes(sim), zoo_params@species_params$species, zoo_params@w)
+  zoo_abunds <- apply(sweep(sweep(zoo_n, c(2, 3), zoo_size_range, 
+                                   "*"), 3, zoo_params@dw, "*"), 
+                       c(1, 2), sum)
+  fish_size_range <- get_size_range_array(sim@params, ...)
+  fish_abunds <- apply(sweep(sweep(sim@n, c(2, 3), fish_size_range, 
+                                    "*"), 3, sim@params@dw, "*"), 
+                        c(1, 2), sum)
+  return(cbind(zoo_abunds, fish_abunds))
+}
